27f1d9bf29e8d82c62f5c9aa551641b1b73382cb
felipepedrosa <felipeepedrosa@gmail.com>
2021-08-29 23:41:39 -0300

    Removed dotenv dependency

--- src/server.js
+++ src/server.js
@@ -1,8 +1,5 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 import app from "./index.js";
 
-app.listen((process.env.PORT ? process.env.PORT : 3333), () => {
+app.listen(9999, () => {
   console.log(`nodejs-api-mock running on port ${process.env.PORT} 🚀`);
 });
