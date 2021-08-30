4e926e632df9e7f9150b606d8d5bd6862667fe99
felipepedrosa <felipeepedrosa@gmail.com>
2021-08-30 13:50:09 -0300

    Refactoring createMethods.js and routes.json.example

--- src/createMethods.js
+++ src/createMethods.js
@@ -2,76 +2,25 @@ import { Router } from "express";
 
 const router = Router();
 
-export function createRoutes(endpointList) {
-  if (!endpointList) throw Error("Endopoint list is required!");
+export function createRoutes(routes) {
+  if (!routes) throw Error("Endopoint list is required!");
 
-  endpointList.forEach((endpoint) => {
-    endpoint.methods.forEach((method) => {
-      const { headers, status, responseBody, responseDelay } = method;
+  routes.forEach((route) => {
+    route.methods.forEach((httpMethod) => {
+      const { responseHeaders, responseStatus, responseBody, responseDelay } =
+        httpMethod;
 
-      switch (method.type.toLowerCase()) {
-        case "get":
-          router.get(endpoint.url, (request, response) =>
-            configureResponse(
-              response,
-              headers,
-              status,
-              responseBody,
-              responseDelay
-            )
-          );
-          break;
-
-        case "post":
-          router.post(endpoint.url, (request, response) =>
-            configureResponse(
-              response,
-              headers,
-              status,
-              responseBody,
-              responseDelay
-            )
-          );
-          break;
-
-        case "patch":
-          router.patch(endpoint.url, (request, response) =>
-            configureResponse(
-              response,
-              headers,
-              status,
-              responseBody,
-              responseDelay
-            )
-          );
-          break;
-
-        case "put":
-          router.put(endpoint.url, (request, response) =>
-            configureResponse(
-              response,
-              headers,
-              status,
-              responseBody,
-              responseDelay
-            )
-          );
-          break;
-
-        case "delete":
-          router.delete(endpoint.url, (request, response) =>
-            configureResponse(
-              response,
-              headers,
-              status,
-              responseBody,
-              responseDelay
-            )
-          );
-          break;
-        default:
-          throw Error(`http method ${method.type} not implemented!`);
-      }
+      router[httpMethod.type.toLowerCase()](
+        route.url,
+        (request, httpResponse) =>
+          configureResponse(
+            httpResponse,
+            responseHeaders,
+            responseStatus,
+            responseBody,
+            responseDelay
+          )
+      );
     });
   });
 
